b98beb90934777a884580976c634d166cb50560d
404kevnotfound <student@HSiMac4-64.local>
2018-10-11 09:37:51 -0600

    Added directory "Chapter 1" including chapter 1 assignments

---
+++ Chapter 1/Programming Exercise 1-9.java
@@ -0,0 +1,10 @@
+class Untitled {
+	public static void main(String[] args) {
+		double width = 4.5;
+		double height = 7.9;
+		System.out.println("Perimeter of the rectangle is:");
+		System.out.println(width * 2 + height * 2);
+		System.out.println("Area of the rectangle is:");
+		System.out.println(width * height);
+	}
+}
